998b97086b0ec6c16e1577a078cd22426fa32efc
David Mosca <david.mosca69@gmail.com>
2021-01-14 00:09:30 +0100

    add auth token to localStorage

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,34 +1,50 @@
-// import axios from 'axios';
-// import { Button } from 'bootstrap';
-// import { Input, Label } from 'reactstrap';
+import axios from 'axios';
+import { useState } from 'react';
+import { Button, FormGroup, Input, Label, Form } from 'reactstrap';
 
 const Auth = () => {
-  // const handleSubmit = () => {
-  //   axios.post('localhost:5000/api/v1/auth', (err, result) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //     console.log(result);
-  //     localStorage.setItem('token', result.data.token);
-  //   });
-  // };
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    const datas = {
+      email,
+      password,
+    };
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/v1/auth', datas)
+      .then((response) => {
+        localStorage.setItem('token', response.data.token);
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      });
+  };
   return (
-    // <div method="post" onSubmit={() => handleSubmit()}>
-    <div>
-      {/* <div>
+    <Form method="post" onSubmit={(e) => handleSubmit(e)}>
+      <FormGroup>
         <Label htmlFor="email">Email : </Label>
-        <Input type="email" name="email" id="email" placeholder="email" />
-      </div>
-      <div>
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
         <Label htmlFor="password">Password : </Label>
         <Input
           type="password"
           name="password"
           id="password"
           placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-      </div> */}
-    </div>
+      </FormGroup>
+      <Button type="submit">Se connecter</Button>
+    </Form>
   );
 };
 
