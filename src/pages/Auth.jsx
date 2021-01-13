a6a1dd85c11d69e0513d8a86e14883f61a0daaf8
David Mosca <david.mosca69@gmail.com>
2021-01-13 22:47:09 +0100

    commit before cloning repo

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,35 @@
+// import axios from 'axios';
+// import { Button } from 'bootstrap';
+// import { Input, Label } from 'reactstrap';
+
 const Auth = () => {
-  return <div />;
+  // const handleSubmit = () => {
+  //   axios.post('localhost:5000/api/v1/auth', (err, result) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(result);
+  //     localStorage.setItem('token', result.data.token);
+  //   });
+  // };
+  return (
+    // <div method="post" onSubmit={() => handleSubmit()}>
+    <div>
+      {/* <div>
+        <Label htmlFor="email">Email : </Label>
+        <Input type="email" name="email" id="email" placeholder="email" />
+      </div>
+      <div>
+        <Label htmlFor="password">Password : </Label>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+        />
+      </div> */}
+    </div>
+  );
 };
 
 export default Auth;
