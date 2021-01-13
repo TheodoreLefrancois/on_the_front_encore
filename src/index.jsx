45f3e0465b67f83a6ce47f2b81400bd172ce4dc2
“Linda <linda.resseguier@gmail.com>
2021-01-13 18:04:29 +0100

    reactstrap and react dom installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
