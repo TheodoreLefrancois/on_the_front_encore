e01d9d10b90b8db06e062a01c6ec3fed3439cf3b
DavidDvpt <david.mosca69@gmail.com>
2021-01-15 12:53:47 +0100

    uploads works fine !!!

--- src/components/PinPopup.jsx
+++ src/components/PinPopup.jsx
@@ -1,24 +1,85 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Popup } from 'react-leaflet';
 import { Form, Input, Label, Button } from 'reactstrap';
 
 const PinPopup = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [file, setFile] = useState([]);
+  const [long] = useState(50.254);
+  const [lat] = useState(-2.54);
+  const [roadTripId] = useState(1);
+
   const handleSubmit = (event) => {
-    // eslint-disable-next-line no-console
-    console.log(event);
+    const formData = new FormData();
+    // console.log(file);
+    formData.append('media', file);
+    const data = {
+      title,
+      long,
+      lat,
+      description,
+      roadTripId,
+    };
+    event.preventDefault();
+
+    axios.post('http://localhost:5000/api/v1/pin', data).then((response) => {
+      // eslint-disable-next-line no-console
+      console.log(response.data);
+      axios.post(
+        `http://localhost:5000/api/v1/media/${response.data.id}/file`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+    });
   };
+
   return (
     <Popup>
-      <Form method="post" onSubmit={(e) => handleSubmit(e)}>
+      <Form
+        method="post"
+        enctype="multipart/form-data"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div>
+          <Label htmlFor="text">Title : </Label>
+          <Input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <Label htmlFor="description">Description : </Label>
+          <Input
+            type="text"
+            name="description"
+            value={description}
+            className="inputPopup"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
         <div>
           <Label htmlFor="file">Select image : </Label>
           <Input
             type="file"
             name="file"
             enctype="multipart/form-data"
-            accept=".jpg,.jpeg,.png"
+            onChange={(e) => {
+              setFile(e.target.files[0]);
+            }}
           />
-          <Button type="submit">Upload</Button>
         </div>
+        <Button type="submit">Send</Button>
       </Form>
     </Popup>
   );
